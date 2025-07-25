5b49f4a30a22898a7711a443b662cd69bcf669ba
ThiagoBD <thiagobarrosdias@gmail.com>
2025-07-25 16:59:29 -0300

    Start Project

---
+++ src/main/java/br/com/thiagodias/teste_deliver/TesteDeliverApplication.java
@@ -0,0 +1,13 @@
+package br.com.thiagodias.teste_deliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteDeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteDeliverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/thiagodias/teste_deliver/TesteDeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.thiagodias.teste_deliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteDeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
